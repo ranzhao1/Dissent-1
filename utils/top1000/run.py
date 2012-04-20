bf95461a8d3cfcf54cfd6aa1f01c7725f62e24f6
Henry Corrigan-Gibbs <henry.corrigan-gibbs.remove-this-text@yale.remove-this-text.edu>
2012-04-20 15:31:52 -0400

    Updating run script

--- utils/top1000/run.py
+++ utils/top1000/run.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python
 
 import os
+import shutil
+import subprocess
 import sys
+import tempfile
 
 def main():
+
   for line in sys.stdin:
+    tmpdir = tempfile.mkdtemp()
+   
     (rank,url) = line.split(',')
-    http = "http://www.%s/" % url.strip()
-    sys.stdout.write("%s..." % http)
+    http = "http://%s/" % url.strip()
+    sys.stdout.write("%s,%s," % (rank,http))
     sys.stdout.flush()
 
-    res = os.system("wget -q -E -H -p -P /tmp/wget/ -U \"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:14.0) Gecko/20120405 Firefox/14.0a1\" %s" % http)
-    if res == 0:
-      print "OK"
-    else:
-      print "Failed"
-      return 
+    args = ["wget", 
+        "-q", "-E", "-H", "-p", "-nd", \
+        "-P", tmpdir, \
+        "-U", "\"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:14.0) Gecko/20120405 Firefox/14.0a1\"",
+        http]
+    subprocess.check_call(args)
+
+    output = subprocess.check_output(["du", "-b", "-s", tmpdir])
+    (size, path) = output.split() 
+
+    sys.stdout.write("%d\n" % int(size))
+    shutil.rmtree(tmpdir)
 
 if __name__ == "__main__":
   main()
