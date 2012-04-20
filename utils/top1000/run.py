745a398243d2feefc867f4766873bc4e8a1fa2ab
Henry Corrigan-Gibbs <henry.corrigan-gibbs.remove-this-text@yale.remove-this-text.edu>
2012-04-20 15:55:18 -0400

    Updating run script'

--- utils/top1000/run.py
+++ utils/top1000/run.py
@@ -5,6 +5,7 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import time
 
 def main():
 
@@ -12,8 +13,9 @@ def main():
     tmpdir = tempfile.mkdtemp()
    
     (rank,url) = line.split(',')
-    http = "http://%s/" % url.strip()
-    sys.stdout.write("%s,%s," % (rank,http))
+    url = url.strip()
+    http = "http://%s/" % url
+    sys.stdout.write("%s,%s," % (rank,url))
     sys.stdout.flush()
 
     args = ["wget", 
@@ -21,12 +23,15 @@ def main():
         "-P", tmpdir, \
         "-U", "\"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:14.0) Gecko/20120405 Firefox/14.0a1\"",
         http]
+
+    tstart = time.time()
     subprocess.check_call(args)
+    tend = time.time()
 
     output = subprocess.check_output(["du", "-b", "-s", tmpdir])
     (size, path) = output.split() 
 
-    sys.stdout.write("%d\n" % int(size))
+    sys.stdout.write("%d,%f,%f\n" % (int(size), tstart, tend))
     shutil.rmtree(tmpdir)
 
 if __name__ == "__main__":
