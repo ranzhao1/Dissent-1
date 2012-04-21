b33dd85f50c6f86bf406b1d913628f3246da65b7
Henry C-G <henry.FAKEEMAIL@gmail.com>
2012-04-21 08:51:50 -0600

    Fixes to run.py script

--- utils/top1000/run.py
+++ utils/top1000/run.py
@@ -25,36 +25,42 @@ def main():
         "-U", "\"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:14.0) Gecko/20120405 Firefox/14.0a1\"",
         http]
     '''
-    args = ["/users/henrycg/build/mulk", "-H", "-q",
+    args = ["/tmp/mulk", "-H", #"-q",
       "-p", "5",
       "-m", "50",
       "-U", "\"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:14.0) Gecko/20120405 Firefox/14.0a1\"",
-      "http://www.%s/" % http]
+      http if http.startswith("http://") else ("http://www.%s/" % http)]
 
     tstart = time.time()
 
     ok = True
     try:
-      subprocess.check_call(args, cwd=tmpdir)
+      p = subprocess.Popen(args, cwd=tmpdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     except subprocess.CalledProcessError as e:
       sys.stderr.write("%s\n" % e)
       ok = False
 
+    (stdout, stderr) = p.communicate()
+    size = stdout.split("Total size downloaded = ")[-1].split()
+    units = size[1].strip()
+    size = size[0].strip()
+
     tend = time.time()
 
+    """
     shutil.rmtree(os.path.join(tmpdir, ".tmp-mulk"))
+
+    subprocess.check_call(['find', tmpdir, '-type', 'f', '-exec', 'mv', '{}', tmpdir, ';'])
+
     (output, stderr) = subprocess.Popen(["du", "-b", "-s", tmpdir], 
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE).communicate()
       
     (size, path) = output.split() 
-  
-    if int(size) == 4096:
-      ok = False
+    """
 
-    sys.stdout.write("%d,%f,%f,%s\n" % (int(size), tstart, tend, "OK" if ok else "Error(s)"))
+    sys.stdout.write("%s,%s,%f,%f,%s\n" % (size, units, tstart, tend, "OK" if ok else "Error(s)"))
       
-
     shutil.rmtree(tmpdir)
 
 if __name__ == "__main__":
