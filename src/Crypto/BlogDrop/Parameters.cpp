04e9ee172325d0e43dec2908c0891b5947ca35f8
Henry Corrigan-Gibbs <henry.corrigan-gibbs.remove-this-text@yale.remove-this-text.edu>
2012-08-14 13:30:34 -0700

    Adding 0x prefix to remove ambiguity

--- src/Crypto/BlogDrop/Parameters.cpp
+++ src/Crypto/BlogDrop/Parameters.cpp
@@ -16,23 +16,23 @@ namespace BlogDrop {
   {
 #ifdef DISSENT_TEST
     const QByteArray bytes_p = QByteArray::fromHex(
-                               "1ADC5BAB8AA55C5B3277EC87A7383ACFDD581D8A86E71"
+                               "0x1ADC5BAB8AA55C5B3277EC87A7383ACFDD581D8A86E71"
                                "1CE98F1690BF81122EE873C53EC2A0646074B94416CDB"
                                "FBC01FADA1C9D110DBEF1706CEBAC27F7D53C8F");
     const QByteArray bytes_g = QByteArray::fromHex(
-                               "140A22002B9DC16A1F4AD9FE6CEB8548F98F7047EDE02"
+                               "0x140A22002B9DC16A1F4AD9FE6CEB8548F98F7047EDE02"
                                "EEF41A0F8DDD85B25AC551137DDD3A940A33EF6889CC5"
                                "78DA0745F458AF4A9171EA189EA2A39D852C9E5");
 #else
     const QByteArray bytes_p = QByteArray::fromHex(
-                               "1CEB470C95CA446FBDD85B00B06D7CEC03189704005BE"
+                               "0x1CEB470C95CA446FBDD85B00B06D7CEC03189704005BE"
                                "DE7779B56F79057C3552BA74E7B1E9592805EB6B9FD43"
                                "09219B5EC755F0B2C8F65737D76246F4B96B5D55761DD"
                                "8EC30BCA7A15C43EC92216D595B4D718002CE32BB4453"
                                "00D151ED2C212BA411F4725D10F7AE459C67857BCE2AB"
                                "99010052AF9F685F37D1484570D35D0B");
     const QByteArray bytes_g = QByteArray::fromHex(
-                               "80022675C64380BF40EC20A2681C4AD9A04CEB144D89B"
+                               "0x80022675C64380BF40EC20A2681C4AD9A04CEB144D89B"
                                "9865402B25E5491C32732E330CC89D3F5C9D474B4B2EB"
                                "C7B5754A8B083432C388BA601D7BD79B371F6A2ED6A51"
                                "98DA86832DE32AC95F1B8EEEF61D1B16E4C7C84FB7AA4"
